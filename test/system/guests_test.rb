47211ce02eca73c2878bbaaf4551299fc496b88d
Joe Webber <35155442+j-webber@users.noreply.github.com>
2025-04-22 14:28:06 -0400

    added guests and auth

---
+++ test/system/guests_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class GuestsTest < ApplicationSystemTestCase
+  setup do
+    @guest = guests(:one)
+  end
+
+  test "visiting the index" do
+    visit guests_url
+    assert_selector "h1", text: "Guests"
+  end
+
+  test "should create guest" do
+    visit guests_url
+    click_on "New guest"
+
+    fill_in "Email", with: @guest.email
+    fill_in "Name", with: @guest.name
+    click_on "Create Guest"
+
+    assert_text "Guest was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Guest" do
+    visit guest_url(@guest)
+    click_on "Edit this guest", match: :first
+
+    fill_in "Email", with: @guest.email
+    fill_in "Name", with: @guest.name
+    click_on "Update Guest"
+
+    assert_text "Guest was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Guest" do
+    visit guest_url(@guest)
+    accept_confirm { click_on "Destroy this guest", match: :first }
+
+    assert_text "Guest was successfully destroyed"
+  end
+end
